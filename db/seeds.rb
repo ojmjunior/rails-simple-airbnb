08b684ce9ed0a167e8c7960f235922410ed8608c
Oscar Junior <ojmjunior@gmail.com>
2020-11-29 12:58:53 -0300

    Adding picture_url to the flat model (Optional) (step 9)

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,30 +2,34 @@ Flat.create!(
   name: 'Light & Spacious Garden Flat London',
   address: '10 Clifton Gardens London W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  price_per_night: 75,
-  number_of_guests: 3
+  price_per_night: rand(80..100),
+  number_of_guests: rand(1..8),
+  picture_url: 'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=967&q=80'
 )
 
 Flat.create!(
   name: 'Light & Spacious Garden Flat Spain',
   address: '10 Clifton Gardens Spain W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  price_per_night: 85,
-  number_of_guests: 3
+  price_per_night: rand(80..100),
+  number_of_guests: rand(1..8),
+  picture_url: 'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=967&q=80'
 )
 
 Flat.create!(
   name: 'Light & Spacious Garden Flat Portugal',
   address: '10 Clifton Gardens Portugal W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  price_per_night: 65,
-  number_of_guests: 3
+  price_per_night: rand(80..100),
+  number_of_guests: rand(1..8),
+  picture_url: 'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=967&q=80'
 )
 
 Flat.create!(
   name: 'Light & Spacious Garden Flat France',
   address: '10 Clifton Gardens France W9 1DT',
   description: 'A lovely summer feel for 9his spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  price_per_night: 95,
-  number_of_guests: 3
+  price_per_night: rand(80..100),
+  number_of_guests: rand(1..8),
+  picture_url: 'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=967&q=80'
 )
